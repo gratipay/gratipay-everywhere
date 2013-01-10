0c59653e4e1d120b02df183faef6e1519369b61a
Luke Strickland <luke@axxim.net>
2013-01-10 16:11:34 -0600

    Gittip Everywhere now works on GitHub!

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Gittip-Everywhere
 =================
 
 The unofficial Gittip addon for the chrome browser, we enable Gittip tipping icons on many of your favorite sites.
+
+This project was inspired by https://github.com/ironchefpython/gittip-browser-extension
---
+++ chrome/websites/github.js
@@ -0,0 +1,28 @@
+if (document.getElementsByClassName('pagehead-actions').length > 0) {
+
+	// Find and create the elements we'll be using.
+    var author = document.getElementsByClassName('author')[0].textContent.replace(/(^\s+|\s+$)/g,'');
+    var actions = document.getElementsByClassName('pagehead-actions')[0];
+	var buttonContainer = document.createElement('li');
+	var button = document.createElement('a');
+
+	// Add attributes to required containers/elements
+	buttonContainer.id = 'gittip-button';
+
+	button.className += 'minibutton js-toggler-target starred';
+	button.rel = 'nofollow';
+	button.target = 'blank';
+	button.href = 'https://www.gittip.com/on/github/'+author;
+	button.innerHTML = '<span class="mini-icon mini-icon-gittip"></span> Gittip';
+
+	// Fill the data
+	buttonContainer.innerHTML = outerHTML(button); 
+
+	// Insert our button!
+	actions.insertBefore(buttonContainer, actions.firstChild);
+}
+
+// Misc function for returning the HTML of a Javascript Element
+function outerHTML(node){
+    return node.outerHTML || new XMLSerializer().serializeToString(node);
+}
