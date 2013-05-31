344b9599c923d40fe02b74ece9dd9a76f0aa27a7
Luke Strickland <luke@axxim.net>
2013-05-31 14:07:10 -0500

    Adding some error handling to the Twitter Widget

--- chrome/websites/twitter/twitter.js
+++ chrome/websites/twitter/twitter.js
@@ -11,27 +11,44 @@
         //var html = load(chrome.extension.getURL('websites/twitter/widget.html'));
 
         // Setup the box areas
-        var profileBox = $('.profile-nav').parent();
-        var gittipBox = document.createElement('div');
-        gittipBox.id = 'gittip';
+        var $profileBox = $('.profile-nav').parent();
+        var $gittipBox = document.createElement('div');
+        $gittipBox.id = 'gittip';
 
-        profileBox.append(gittipBox);
+        $profileBox.append($gittipBox);
 
         // Let's "create" our new box.
         $('#gittip').load(chrome.extension.getURL('websites/twitter/widget.html'), function(widget) {
-            $.get('https://www.gittip.com/'+username+'/public.json', function(data) {
-                gittip = data;
-                console.log(data);
-                
-                widget = widget.replace('{{RECEIVE}}', gittip.receiving);
-                widget = widget.replace('{{GIVING}}', gittip.giving);
-                widget = widget.replace('{{FULLNAME}}', fullname);
-                widget = widget.replace('{{USERNAME}}', username);
-                widget = widget.replace('{{USERNAME}}', username);
 
-                $('#gittip').html(widget);
-                $('.loading').hide();
-                $('.loaded').show();
+            $.ajax({
+                type: 'GET',
+                url: 'https://www.gittip.com/'+username+'/public.json',
+                beforeSend:function(){
+                    // this is where we append a loading image
+                    
+                },
+                success:function(data){
+                    // successful request; do something with the data
+                    gittip = data;
+
+                    widget = widget.replace('{{RECEIVE}}', gittip.receiving);
+                    widget = widget.replace('{{GIVING}}', gittip.giving);
+                    widget = widget.replace('{{FULLNAME}}', fullname);
+                    widget = widget.replace('{{USERNAME}}', username);
+                    widget = widget.replace('{{USERNAME}}', username);
+
+                    $('#gittip').html(widget);
+                    $('.loading').hide();
+                    $('.loaded').show();
+                },
+                error: function(data){
+                    console.log(data);
+                    // If we're here, this means Gittip broke, so we should try and load the "on/twitter"
+
+                    // Since Gittip doesn't have an API on it atm, we'll just hide the box.
+                    $('#gittip').hide();
+
+                }
             });
         });
 
