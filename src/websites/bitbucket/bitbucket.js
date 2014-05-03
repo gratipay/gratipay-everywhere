0914af46b63e3091b46f5ce1c51fa46dc14b97ce
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-05-03 19:33:09 +0000

    Update bitbucket theming.

--- src/websites/bitbucket/bitbucket.js
+++ src/websites/bitbucket/bitbucket.js
@@ -4,7 +4,7 @@ var bitbucket = function() {
 	var author = $('.user').children('span')[1].innerText;
 	var buttonParent = $('#repo-toolbar').children('.aui-buttons')[0];
 
-	$(buttonParent).append('<a id="gittip-button" class="aui-button aui-style" href="https://www.gittip.com/'+author+'"><span class="aui-icon icon gittip">Tip on Gittip!</span><span>Gittip</span></a>');
+	$(buttonParent).append('<a id="gittip-button" class="aui-button" href="https://www.gittip.com/'+author+'"><span class="aui-icon aui-icon-small aui-iconfont-gittip"></span><span>Gittip</span></a>');
 
 };
 
