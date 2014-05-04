dad4c65ae5ad9e4a5fc3f2506c31b6c78107e411
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-05-04 16:10:21 +0000

    Implement forge.tools.getURL call for twitter.

--- src/websites/twitter/twitter.js
+++ src/websites/twitter/twitter.js
@@ -7,8 +7,6 @@
         var username = $('link[rel="canonical"]').attr('href').replace('https://twitter.com/','');
         var fullname = $('title').text().replace(' ('+username+') on Twitter','');
 
-        //var html = load(forge.tools.getURL('src/websites/twitter/widget.html'));
-
         // Setup the box areas
         var $profileBox = $('.profile-nav').parent();
         var $gittipBox = document.createElement('div');
@@ -17,42 +15,42 @@
         $profileBox.append($gittipBox);
 
         // Let's "create" our new box.
-        // forge.tools.getURL is not working here for some reason... TODO
-        $('#gittip').load(chrome.extension.getURL('src/websites/twitter/widget.html'), function(widget) {
+        forge.tools.getURL('websites/twitter/widget.html', function(uri) {
+            console.log(uri);
+            $('#gittip').load(uri, function(widget) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://www.gittip.com/on/twitter/'+username+'/public.json',
+                    beforeSend:function(){
+                        // this is where we append a loading image
+                    },
+                    success:function(data){
+                        // stop here if they aren't on Gittip
+                        if (data.on != 'gittip')
+                            return this.error(data);
 
-            $.ajax({
-                type: 'GET',
-                url: 'https://www.gittip.com/on/twitter/'+username+'/public.json',
-                beforeSend:function(){
-                    // this is where we append a loading image
-                },
-                success:function(data){
-                    // stop here if they aren't on Gittip
-                    if (data.on != 'gittip')
-                        return this.error(data);
+                        // successful request; do something with the data
+                        gittip = data;
 
-                    // successful request; do something with the data
-                    gittip = data;
+                        widget = widget.replace('{{RECEIVE}}', gittip.receiving);
+                        widget = widget.replace('{{GIVING}}', gittip.giving);
+                        widget = widget.replace('{{FULLNAME}}', fullname);
+                        widget = widget.replace('{{USERNAME}}', username);
+                        widget = widget.replace('{{USERNAME}}', username);
 
-                    widget = widget.replace('{{RECEIVE}}', gittip.receiving);
-                    widget = widget.replace('{{GIVING}}', gittip.giving);
-                    widget = widget.replace('{{FULLNAME}}', fullname);
-                    widget = widget.replace('{{USERNAME}}', username);
-                    widget = widget.replace('{{USERNAME}}', username);
+                        $('#gittip').html(widget);
+                        $('.loading').hide();
+                        $('.loaded').show();
+                    },
+                    error: function(data){
+                        console.log(data);
+                        // for now just hide it all, but we should check to see if they exist, and
+                        // are just not on Gittip, or if we're getting a 404
+                        $('#gittip').hide();
 
-                    $('#gittip').html(widget);
-                    $('.loading').hide();
-                    $('.loaded').show();
-                },
-                error: function(data){
-                    console.log(data);
-                    // for now just hide it all, but we should check to see if they exist, and
-                    // are just not on Gittip, or if we're getting a 404
-                    $('#gittip').hide();
-
-                }
+                    }
+                });
             });
-
         });
 
     }
