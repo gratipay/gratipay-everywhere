6a53b763f0861d7ca68eb1e558c8c8f2d6d08faa
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-05-05 06:14:45 +0000

    Swap side of Gittip button on Bitbucket.
    For visual consistency and aesthetic after site theming change.

--- src/websites/bitbucket/bitbucket.js
+++ src/websites/bitbucket/bitbucket.js
@@ -4,7 +4,7 @@ var bitbucket = function() {
 	var author = $('.user').children('span')[1].innerText;
 	var buttonParent = $('#repo-toolbar').children('.aui-buttons')[0];
 
-	$(buttonParent).append('<a id="gittip-button" class="aui-button" href="https://www.gittip.com/'+author+'"><span class="aui-icon aui-icon-small aui-iconfont-gittip"></span><span>Gittip</span></a>');
+	$(buttonParent).prepend('<a id="gittip-button" class="aui-button" href="https://www.gittip.com/'+author+'"><span class="aui-icon aui-icon-small aui-iconfont-gittip"></span><span>Gittip</span></a>');
 
 };
 
